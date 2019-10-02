d236af72dacccc0df66aea6a6ea96ebf79711857
Rob Wakeman <rob@robwakeman.com>
2019-10-02 14:18:03 +0100

    convert nav sass to bem

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,18 +3,18 @@ import { Link } from "gatsby"
 import navStyles from "./nav.module.scss"
 
 const Nav = () => (
-  <nav className={navStyles.navMenu}>
-    <ul className={navStyles.navList}>
+  <nav className={navStyles.nav}>
+    <ul className={navStyles.nav__list}>
       <li className={navStyles.navItem}>
         <Link to="/">Home</Link>
       </li>
-      <li className={navStyles.navItem}>
+      <li className={navStyles.nav__item}>
         <Link to="/portfolio">Portfolio</Link>
       </li>
-      <li className={navStyles.navItem}>
+      <li className={navStyles.nav__item}>
         <Link to="/about">About</Link>
       </li>
-      <li className={navStyles.navItem}>
+      <li className={navStyles.nav__item}>
         <Link to="/contact">Contact</Link>
       </li>
     </ul>
