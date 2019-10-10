f6057aae9551fa5b03c72efa62bdc2dc8a3b9c20
Rob Wakeman <rob@robwakeman.com>
2019-10-11 00:01:27 +0100

    integrate foll-and styles into css modules

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,32 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 import navStyles from "./nav.module.scss"
 
-// const Nav = () => (
-//   <nav className={navStyles.nav}>
-//     <ul className={navStyles.nav__list}>
-//       <li className={navStyles.navItem}>
-//         <Link to="/">Home</Link>
-//       </li>
-//       <li className={navStyles.nav__item}>
-//         <Link to="/portfolio">Portfolio</Link>
-//       </li>
-//       <li className={navStyles.nav__item}>
-//         <Link to="/about">About</Link>
-//       </li>
-//       <li className={navStyles.nav__item}>
-//         <Link to="/contact">Contact</Link>
-//       </li>
-//     </ul>
-//   </nav>
-// )
-
 const Nav = () => (
   <>
-    <input className="menu-btn" type="checkbox" id="menu-btn" />
-    <label className="menu-icon" for="menu-btn">
-      <span className="nav-icon"></span>
+    <input className={navStyles.menuBtn} type="checkbox" id="menu-btn" />
+    <label className={navStyles.menuIcon} htmlFor="menu-btn">
+      <span className={navStyles.navIcon}></span>
     </label>
-    <ul className="menu">
+    <ul className={navStyles.menu}>
       <li>
         <Link to="/">Home</Link>
       </li>
