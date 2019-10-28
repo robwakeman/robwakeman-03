30751f3d05714b728cc733fe9d73c078c6608420
Rob Wakeman <rob@robwakeman.com>
2019-10-28 14:01:08 +0000

    add nav element to nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,24 +4,26 @@ import navStyles from "./nav.module.scss"
 
 const Nav = () => (
   <>
-    <input className={navStyles.menuBtn} type="checkbox" id="menu-btn" />
-    <label className={navStyles.menuIcon} htmlFor="menu-btn">
-      <span className={navStyles.navIcon}></span>
-    </label>
-    <ul className={navStyles.menu}>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
+    <nav>
+      <input className={navStyles.menuBtn} type="checkbox" id="menu-btn" />
+      <label className={navStyles.menuIcon} htmlFor="menu-btn">
+        <span className={navStyles.navIcon}></span>
+      </label>
+      <ul className={navStyles.menu}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   </>
 )
 
