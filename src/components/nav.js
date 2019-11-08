c6bd725b314ff2b9f42c6fc1eecce61ed59f5e98
Rob Wakeman <rob@robwakeman.com>
2019-11-08 14:59:26 +0000

    remove coment in nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import navStyles from "./nav.module.scss"
 
 const Nav = () => {
-  // js stuff here
   const [navIsOpen, setNavOpenState] = useState(false)
 
   function toggleNav() {
