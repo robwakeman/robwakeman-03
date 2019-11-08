a26d71e190c86bbdd974fdbacf5a3b2d56a6fa42
Rob Wakeman <rob@robwakeman.com>
2019-11-08 13:00:18 +0000

    modify Nav component to allow JS before return statement

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,42 +2,48 @@ import React from "react"
 import { Link } from "gatsby"
 import navStyles from "./nav.module.scss"
 
-const Nav = () => (
-  <>
-    <nav className={navStyles.nav}>
-      {/* <input className={navStyles.menuBtn} type="checkbox" id="menu-btn" /> */}
-      {/* <label className={navStyles.menuIcon} htmlFor="menu-btn">
-        <span className={navStyles.navIcon}></span>
-      </label> */}
-      <ul className={navStyles.navItems}>
-        <li className={navStyles.navItem}>
-          <Link to="/" className={navStyles.navLink}>
-            Home
-          </Link>
-        </li>
-        <li className={navStyles.navItem}>
-          <Link to="/portfolio" className={navStyles.navLink}>
-            Portfolio
-          </Link>
-        </li>
-        <li className={navStyles.navItem}>
-          <Link to="/about" className={navStyles.navLink}>
-            About
-          </Link>
-        </li>
-        <li className={navStyles.navItem}>
-          <Link to="/contact" className={navStyles.navLink}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-      <div className={navStyles.burger}>
-        <div className={navStyles.line1}></div>
-        <div className={navStyles.line2}></div>
-        <div className={navStyles.line3}></div>
-      </div>
-    </nav>
-  </>
-)
+const Nav = () => {
+  // js stuff here
+  // const [count, setCount] = useState(0);
+  const foo = true
+
+  return (
+    <>
+      <nav className={navStyles.nav}>
+        {/* <input className={navStyles.menuBtn} type="checkbox" id="menu-btn" /> */}
+        {/* <label className={navStyles.menuIcon} htmlFor="menu-btn">
+          <span className={navStyles.navIcon}></span>
+        </label> */}
+        <ul className={navStyles.navItems}>
+          <li className={navStyles.navItem}>
+            <Link to="/" className={navStyles.navLink}>
+              Home
+            </Link>
+          </li>
+          <li className={navStyles.navItem}>
+            <Link to="/portfolio" className={navStyles.navLink}>
+              Portfolio
+            </Link>
+          </li>
+          <li className={navStyles.navItem}>
+            <Link to="/about" className={navStyles.navLink}>
+              About
+            </Link>
+          </li>
+          <li className={navStyles.navItem}>
+            <Link to="/contact" className={navStyles.navLink}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className={navStyles.burger}>
+          <div className={navStyles.line1}></div>
+          <div className={navStyles.line2}></div>
+          <div className={navStyles.line3}></div>
+        </div>
+      </nav>
+    </>
+  )
+}
 
 export default Nav
