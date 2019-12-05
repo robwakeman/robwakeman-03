fa0f09ea9be6f625c04cb9bd91fe8d51edfe09af
Rob Wakeman <rob@robwakeman.com>
2019-12-05 13:41:05 +0000

    add sections to index.js, h2 style, edit  content in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,47 +10,66 @@ import layoutStyles from "../components/layout.module.scss"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>I'm Rob, a front-end developer</h1>
-    <p>
-      Based in London, I work with designers and clients to{" "}
-      <Link to="/work/">produce websites</Link> that work for users and meet
-      business goals.
-    </p>
-    <Link to="/contact/" className={layoutStyles.cta}>
-      Get in touch
-    </Link>
-    <p>I care strongly about user experience.</p>
-    <p>
-      Clients include: Tate and Central Saint Martins (University of the Arts
-      London)
-    </p>
+    <section className={`${layoutStyles.section} ${layoutStyles.intro}`}>
+      <h1>I'm Rob, a front-end developer</h1>
+      <p>
+        Based in London, I work with designers and clients to{" "}
+        <Link to="/work/">produce websites</Link> that work for users and meet
+        business goals.
+      </p>
+      <Link to="/contact/" className={layoutStyles.cta}>
+        Get in touch
+      </Link>
+      <p>I care strongly about user experience.</p>
+      <p>
+        Clients include: Tate and Central Saint Martins (University of the Arts
+        London)
+      </p>
+    </section>
 
-    <h3>Commercial experience with:</h3>
-    <ul>
-      <li>HTML</li>
-      <li>CSS/Sass</li>
-      <li>JavaScript</li>
-      <li>jQuery</li>
-      <li>WordPress</li>
-      <li>Bootstrap</li>
-      <li>Task runners</li>
-      <li>Git</li>
-    </ul>
+    <section className={`${layoutStyles.section} ${layoutStyles.skills}`}>
+      <h2>Commercial experience with:</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS/Sass</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>WordPress</li>
+        <li>Bootstrap</li>
+        <li>Task runners</li>
+        <li>Git</li>
+      </ul>
 
-    <h3>Other technologies I have used:</h3>
-    <ul>
-      <li>React</li>
-      <li>Gatsby</li>
-      <li>Angular</li>
-      <li>PHP</li>
-    </ul>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe veritatis
-      rem itaque corrupti quia molestiae cumque. Facilis voluptate at.{" "}
-      <a href="https://www.robwakeman.com">Here is a link</a> laudantium earum
-      ipsum, fugiat, suscipit dolorum nostrum quisquam accusamus, ipsam culpa
-      fugit veritatis. Odio, soluta nihil exercitationem.
-    </p>
+      <h2>Other technologies used:</h2>
+      <ul>
+        <li>React</li>
+        <li>Gatsby</li>
+        <li>Angular</li>
+        <li>PHP</li>
+      </ul>
+    </section>
+
+    <section className={`${layoutStyles.section} ${layoutStyles.skillsDemo}`}>
+      <p>
+        HTML, CSS, JavaScript skills demonstrated on:
+        <ul>
+          <li>
+            <a href="https://github.com/robwakeman/book-reading-list">
+              book-reading-list
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/robwakeman/npm-scripts-igloo">
+              npm-scripts-igloo
+            </a>
+          </li>
+        </ul>
+        laudantium earum ipsum, fugiat, suscipit dolorum nostrum quisquam
+        accusamus, ipsam culpa fugit veritatis. Odio, soluta nihil
+        exercitationem.
+      </p>
+    </section>
+
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quia at
       nobis aliquid officia blanditiis provident vero enim repellat, inventore.
