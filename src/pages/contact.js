66d3fe599bc89e75ddd933c1ebd75327ed42ba02
Rob Wakeman <rob@robwakeman.com>
2019-12-16 12:20:57 +0000

    add dummy contact  page to prevent console errors when gatsby build gatsby serve

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,12 +6,12 @@ import SEO from "../components/seo"
 
 import layoutStyles from "../components/layout.module.scss"
 
-const AboutPage = () => (
+const ContactPage = () => (
   <Layout>
-    <SEO title="About" />
-    <h1>About</h1>
-    <p>This is the About page</p>
+    <SEO title="Contact" />
+    <h1>Contact</h1>
+    <p>This is the Contact page</p>
   </Layout>
 )
 
-export default AboutPage
+export default ContactPage
