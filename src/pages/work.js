788a064a124b55bee84d7194766e870f57cefa66
Rob Wakeman <rob@robwakeman.com>
2019-12-17 23:40:07 +0000

    change li elements into divs for work list

--- src/pages/work.js
+++ src/pages/work.js
@@ -36,79 +36,75 @@ const WorkPage = () => {
         React, Gatsby, WordPress:
       </p>
 
-      <ul>
-        <li>
-          <a
-            href="https://github.com/robwakeman/book-reading-list"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <b>robwakeman-03 (TO AMEND LINK)</b>
-          </a>
-          <p>Rob Wakeman personal site built in Gatsby</p>
-        </li>
-        <li>
-          <a
-            href="https://github.com/robwakeman/book-reading-list"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <b>book-reading-list</b>
-          </a>
-          <p>Book reading list app</p>
-        </li>
-        <li>
-          <a
-            href="https://github.com/robwakeman/short-jokes"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <b>short-jokes</b>
-          </a>
-          <p>React app that displays short jokes</p>
-        </li>
-        <li>
-          <a
-            href="https://github.com/robwakeman/npm-scripts-igloo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <b>npm-scripts-igloo</b>
-          </a>
-          <p>
-            Student and teacher list generator, CSS animations, npm scripts
-            build process with Sass and Babel
-          </p>
-        </li>
-        <li>
-          <a
-            href="https://github.com/robwakeman/rwajaxform"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <b>rwajaxform</b>
-          </a>
-          <p>
-            Custom WordPress contact form plugin integrated with Bootstrap 4
-            validation
-          </p>
-        </li>
-      </ul>
+      <div>
+        <a
+          href="https://github.com/robwakeman/book-reading-list"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>robwakeman-03 (TO AMEND LINK)</b>
+        </a>
+        <p>Rob Wakeman personal site built in Gatsby</p>
+      </div>
+      <div>
+        <a
+          href="https://github.com/robwakeman/book-reading-list"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>book-reading-list</b>
+        </a>
+        <p>Book reading list app</p>
+      </div>
+      <div>
+        <a
+          href="https://github.com/robwakeman/short-jokes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>short-jokes</b>
+        </a>
+        <p>React app that displays short jokes</p>
+      </div>
+      <div>
+        <a
+          href="https://github.com/robwakeman/npm-scripts-igloo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>npm-scripts-igloo</b>
+        </a>
+        <p>
+          Student and teacher list generator, CSS animations, npm scripts build
+          process with Sass and Babel
+        </p>
+      </div>
+      <div>
+        <a
+          href="https://github.com/robwakeman/rwajaxform"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>rwajaxform</b>
+        </a>
+        <p>
+          Custom WordPress contact form plugin integrated with Bootstrap 4
+          validation
+        </p>
+      </div>
 
       <h2>Earlier client work</h2>
 
-      <ul>
-        {data.allMarkdownRemark.edges.map(project => {
-          return (
-            <li key={project.node.id}>
-              <Link to={project.node.frontmatter.path}>
-                <b>{project.node.frontmatter.title}</b>
-              </Link>
-              <p>{project.node.frontmatter.description}</p>
-            </li>
-          )
-        })}
-      </ul>
+      {data.allMarkdownRemark.edges.map(project => {
+        return (
+          <div key={project.node.id}>
+            <Link to={project.node.frontmatter.path}>
+              <b>{project.node.frontmatter.title}</b>
+            </Link>
+            <p>{project.node.frontmatter.description}</p>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
