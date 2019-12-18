4990fffb74bd22b4199e506b2af0ca6e92604f05
Rob Wakeman <rob@robwakeman.com>
2019-12-18 17:54:33 +0000

    create pages for all project md files, simple project template so far, to be developed

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const projectTemplate = path.resolve("./src/templates/project.js")
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            html
+            frontmatter {
+              path
+              title
+              description
+              client
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const projectPages = result.data.allMarkdownRemark.edges
+
+  projectPages.forEach(edge => {
+    createPage({
+      path: edge.node.frontmatter.path,
+      component: projectTemplate,
+    })
+  })
+}
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,27 +1,34 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Project = () => {
-  const data = useStaticQuery(graphql`
-    query TestQuery {
-      site {
-        siteMetadata {
-          title
-          description
-        }
-      }
-    }
-  `)
+const Project = ({ data }) => {
+  const { markdownRemark: project } = data
 
   return (
     <Layout>
       {/* Make this SEO title attribute dynamic */}
       <SEO title="Project" />
-      <p>Project template - and the data is: {data.site.siteMetaData.title}</p>
+      <h1>{project.frontmatter.title}</h1>
+      <p>{project.frontmatter.description}</p>
+      <p>{project.frontmatter.client}</p>
     </Layout>
   )
 }
+
+export const data = graphql`
+  query ProjectByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+        description
+        client
+      }
+    }
+  }
+`
 
 export default Project
