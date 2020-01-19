b692d88a20c91859c5ae622360fa0bc98c38671a
Rob Wakeman <rob@robwakeman.com>
2020-01-19 10:49:54 +0000

    remove import image-astronaut in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import ImageAstronaut from "../components/image-astronaut"
 import SEO from "../components/seo"
 
 import layoutStyles from "../components/layout.module.scss"
