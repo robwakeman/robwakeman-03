ae2bff652d78961765efabd4b418f2e1ca302b6e
Rob Wakeman <rob@robwakeman.com>
2020-01-19 10:48:06 +0000

    edit copy in about.js, style testimonial

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,9 +25,23 @@ const AboutPage = () => (
     </p>
     <p>
       For clients I deliver high quality work on time with excellent
-      communication throughout the project lifecycle.
+      communication throughout the project lifecycle. And I offer ongoing
+      support and guidance after sign off.
     </p>
-    <p>And I offer ongoing support and guidance after sign off.</p>
+    <figure>
+      <blockquote cite="Client: Central Saint Martins – UAL">
+        <p>
+          Rob has fantastic attention to detail and thoroughness in his
+          approach, which I greatly appreciate. His technical knowledge and
+          problem-solving abilities have been invaluable in meeting any request
+          or challenge I have put to him, and in delivering a site that meets
+          our needs.
+        </p>
+      </blockquote>
+      <figcaption>
+        <cite>Client: Central Saint Martins – UAL</cite>
+      </figcaption>
+    </figure>
     <Link to="/contact/" className={layoutStyles.cta}>
       Get in touch
     </Link>
