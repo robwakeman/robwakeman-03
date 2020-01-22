63687cf57a9fce7eee656b7815ea144605386d44
Rob Wakeman <rob@robwakeman.com>
2020-01-22 18:52:40 +0000

    remove minireset.scss as an import from the options in gatsby-plugin-sass in gatsby-config.js, copy contents of minireset to top of layout.module, restore body rule of margin & padding 0

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        data: `@import "variables";@import "mixins";@import "minireset";`,
+        data: `@import "variables";@import "mixins";`,
         includePaths: [`${__dirname}/src/styles`],
       },
     },
