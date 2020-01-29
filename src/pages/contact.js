a012dc15cd5d012297749d2af8604bc8a03a7c0e
Rob Wakeman <rob@robwakeman.com>
2020-01-29 14:23:14 +0000

    add info about form, form yet to be implemented

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,14 +6,10 @@ const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact</h1>
+    <p>This is the Contact page.</p>
     <p>
-      This is the Contact page. Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Cumque beatae, aut officia quam error dolore quae!
-      Magnam cum impedit dolores architecto consequuntur quas nemo quia sapiente
-      voluptas fugit aut saepe id sed nam ad magni, distinctio eveniet. Quia
-      culpa, neque delectus sed dolore aut, distinctio eos tempore dolores
-      eveniet nisi officiis, nulla voluptates praesentium autem quasi cum
-      explicabo ducimus.
+      Contact form to go here. Either Formspree or Netlify form if hosted at
+      Netlify.
     </p>
   </Layout>
 )
