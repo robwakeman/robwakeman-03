befb38262d7f177f8e78b4646680d1535bce29e3
Rob Wakeman <rob@robwakeman.com>
2020-01-30 15:00:33 +0000

    amend 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Page Not Found</h1>
+    <p>
+      Maybe try the <Link to="/">home page</Link>
+    </p>
   </Layout>
 )
 
