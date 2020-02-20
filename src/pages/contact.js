d10a84afa96f610dd55894065bc62c5ce707a323
Rob Wakeman <rob@robwakeman.com>
2020-02-20 16:19:37 +0000

    add contact form, markup and css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,64 @@
 import React from "react"
 import Layout from "../components/layout"
+import contactStyles from "./contact.module.scss"
 import SEO from "../components/seo"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Contact</h1>
-    <p>This is the Contact page.</p>
-    <p>
-      Contact form to go here. Either Formspree or Netlify form if hosted at
-      Netlify.
-    </p>
+    <p>Contact form is work in progress...</p>
+    <form
+      className={contactStyles.contactForm}
+      name="contact"
+      method="POST"
+      data-netlify-recaptcha="true"
+      data-netlify="true"
+    >
+      <div className={contactStyles.hidden}>
+        <label>
+          Please ignore this field if you're human: <input name="bot-field" />
+        </label>
+      </div>
+
+      <div className={contactStyles.formGroup}>
+        <label for="name">Your Name</label>
+        <input
+          className={contactStyles.formControl}
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+      </div>
+
+      <div className={contactStyles.formGroup}>
+        <label for="email">Your Email</label>
+        <input
+          className={contactStyles.formControl}
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+      </div>
+
+      <div className={contactStyles.formGroup}>
+        <label for="message">Message</label>
+        <textarea
+          className={contactStyles.formControl}
+          id="message"
+          name="message"
+          placeholder="Your message"
+        ></textarea>
+      </div>
+
+      <div data-netlify-recaptcha="true"></div>
+
+      <div className={contactStyles.formGroup}>
+        <button type="submit">Send</button>
+      </div>
+    </form>
   </Layout>
 )
 
